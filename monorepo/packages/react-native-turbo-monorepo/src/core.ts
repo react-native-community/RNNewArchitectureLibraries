769cef38bb35f029d566e258ee56c3c5342a6b0a
SE <se@develup.pro>
2024-03-16 21:00:22 +0200

    [Packages] add startListening

--- monorepo/packages/react-native-turbo-monorepo/src/core.ts
+++ monorepo/packages/react-native-turbo-monorepo/src/core.ts
@@ -1,5 +1,11 @@
 import { TurboMonorepo } from './TurboMonorepo';
+import { NativeEventEmitter } from 'react-native';
 
 export function multiply(a: number, b: number): Promise<number> {
   return TurboMonorepo.multiply(a, b);
+}
+
+export function startListening(listener: (event: any) => void) {
+  const eventEmitter = new NativeEventEmitter(TurboMonorepo);
+  return eventEmitter.addListener(`multiplyEvent`, listener);
 }
