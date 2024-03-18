f3b5b92723623b951d7b06deb92a00d1bc98d50a
SE <se@develup.pro>
2024-03-18 13:06:16 +0200

    [Examples] remove rn module

--- monorepo/examples/turbo-expo/src/App.js
+++ monorepo/examples/turbo-expo/src/App.js
@@ -1,20 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { multiply, startListening } from 'react-native-turbo-monorepo';
 
 export function App() {
   const [result, setResult] = useState();
-
-  // useEffect(() => {
-  //   const subscribe = startListening((event) => {
-  //     console.log('event', event);
-  //   });
-  //   multiply(3, 7).then(setResult);
-  //
-  //   return () => {
-  //     subscribe.remove();
-  //   };
-  // }, []);
 
   return (
     <View style={styles.container}>
