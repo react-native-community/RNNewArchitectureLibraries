9ce8fc0829eeb5603dedd9c8c60c6adf74b6aab3
SE <se@develup.pro>
2024-03-20 12:13:55 +0200

    [Android] add event listener

--- tsc-event-emitter/android/src/main/java/com/tsceventemitter/TscEventEmitterModule.java
+++ tsc-event-emitter/android/src/main/java/com/tsceventemitter/TscEventEmitterModule.java
@@ -1,16 +1,24 @@
 package com.tsceventemitter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class TscEventEmitterModule extends TscEventEmitterSpec {
   public static final String NAME = "TscEventEmitter";
 
+  private ReactApplicationContext reactContext;
+  private int listenerCount = 0;
+
   TscEventEmitterModule(ReactApplicationContext context) {
     super(context);
+    reactContext = context;
   }
 
   @Override
@@ -19,11 +27,40 @@ public class TscEventEmitterModule extends TscEventEmitterSpec {
     return NAME;
   }
 
+  @ReactMethod
+  public void addListener(String eventName) {
+    if (listenerCount == 0) {
+      // Set up any upstream listeners or background tasks as necessary
+    }
+    listenerCount += 1;
+  }
+
+  @ReactMethod
+  public void removeListeners(double count) {
+    listenerCount -= (int) count;
+    if (listenerCount <= 0) {
+      listenerCount = 0;
+      // Remove upstream listeners, stop unnecessary background tasks
+    }
+  }
+
+  private void sendEvent(String eventName,
+                         @Nullable WritableMap params) {
+    reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, params);
+  }
+
 
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
   public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+    double result = a * b;
+    WritableMap value = Arguments.createMap();
+    value.putDouble("result", result);
+    sendEvent("multiplyEvent", value);
+
+    promise.resolve(result);
   }
 }
